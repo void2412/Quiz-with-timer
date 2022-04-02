e392754d713e69f4e2c5d1fecc91d1cf6a6e81ef
Chi Hieu Nguyen <void2412@gmail.com>
2022-04-02 19:52:02 +1030

    finished highscore button

--- script.js
+++ script.js
@@ -4,9 +4,11 @@ const answerList =[]
 const choiceList =[]
 var timeLeft = 0
 var highscoreList = localStorage.getItem('highscore')
+var highscoreToggle = false
 
-var scoreArea = document.querySelector('#highscoreArea')
+// item selected in HTML DOM
 var scoreList = document.querySelector('#highscore')
+var scoreBtn = document.querySelector('#viewScore')
 
 var questionArea = document.querySelector('#questionArea')
 var question = document.querySelector('#question')
@@ -17,21 +19,36 @@ var playerDetail = document.querySelector('#playerDetail')
 
 var timerArea = document.querySelector('#timerArea')
 var timerText = document.querySelector('#timer')
+var penaltyText = document.querySelector('#penalty')
 
 function init(){
-
+    scoreList.setAttribute('style', 'display:none;')
 }
-
+init()
 // rendering functions
-function showHighscore(){
-
+function toggleHighscore(){
+    if (!highscoreToggle){
+        scoreList.setAttribute('style', 'display: block;')
+        scoreBtn.textContent = "Hide HighScore"
+        highscoreToggle = true
+        // for (var i = 0; i < highscoreList.length; i++){
+            // var liEl = document.createElement('li')
+    
+        // }
+    }
+    else{
+        scoreList.setAttribute('style', 'display: none;')
+        scoreBtn.textContent = "Show HighScore"
+        highscoreToggle = false
+    }
+    
 }
 
 function showQuestion(){
 
 }
 
-function showTime(){
+function showTimeRemaining(){
 
 }
 
@@ -51,3 +68,7 @@ function checkHighscore(){
 function updateHighscore(){
 
 }
+
+// assign event linstener to buttons
+
+scoreBtn.addEventListener('click', toggleHighscore)
