8fdf380bb650da0a70f4c8ef6f89aef4eb172563
Chi Hieu Nguyen <void2412@gmail.com>
2022-04-02 19:13:38 +1030

    added some id, added some blank functions for JS, layout structure

---
+++ script.js
@@ -0,0 +1,53 @@
+// initial variable and constant
+const questionList =[]
+const answerList =[]
+const choiceList =[]
+var timeLeft = 0
+var highscoreList = localStorage.getItem('highscore')
+
+var scoreArea = document.querySelector('#highscoreArea')
+var scoreList = document.querySelector('#highscore')
+
+var questionArea = document.querySelector('#questionArea')
+var question = document.querySelector('#question')
+
+var inputArea = document.querySelector('#inputArea')
+var btnArea = document.querySelector('#questionAnswer')
+var playerDetail = document.querySelector('#playerDetail')
+
+var timerArea = document.querySelector('#timerArea')
+var timerText = document.querySelector('#timer')
+
+function init(){
+
+}
+
+// rendering functions
+function showHighscore(){
+
+}
+
+function showQuestion(){
+
+}
+
+function showTime(){
+
+}
+
+// get Input and process input functions
+function getPlayerDetail(){
+
+}
+
+function checkAnswer(){
+
+}
+
+function checkHighscore(){
+
+}
+
+function updateHighscore(){
+
+}
